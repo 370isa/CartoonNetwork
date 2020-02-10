3c981de0b83ecf786d8d6d1eac9defb0b007b2f2
370isa <eloisaamorin370@gmail.com>
2020-02-10 11:21:53 -0300

    #3: Adiciona carrossel com os Personagens e melhora o bloco dos horários

---
+++ data.js
@@ -0,0 +1,102 @@
+module.exports = [
+  {
+    image: './images/3-espias-demais.png',
+    title: '3 Espiãs Demais',
+    description: 'Sob o cuidado do diretor da Organização Mundial de Proteção Humana, três garotas, de Beverly Hills, se transformam em agentes secretos. Elas combatem o crime internacional e frequentam a escola para manter a qualidade de suas notas.',
+  },
+  {
+    image: './images/avatar.png',
+    title: 'Avatar',
+    description: 'Os irmãos Katara e Sokka acordam o jovem Aang de uma longa hibernação e descobrem que ele é um Avatar, com poderes que podem derrotar a Nação do Fogo.',
+  },
+  {
+    image: './images/bob-esponja.png',
+    title: 'Bob Esponja',
+    description: 'Uma esponja-do-mar chamada Bob Esponja Calça Quadrada mora com seu caracol de estimação na Fenda do Biquíni, no fundo do oceano. Bob trabalha no Siri Cascudo e, nas horas vagas, vive arrumando confusões com seu melhor amigo, a estrela-do-mar Patrick.',
+  },
+  {
+    image: './images/caverna-do-dragao.png',
+    title: 'Caverna do Dragão',
+    description: 'Um grupo de crianças vai a um parque de diversões e lá se deparam com o brinquedo Caverna do Dragão. Ao entrarem nele, são enviados a um mundo mágico, aonde vivem muitas aventuras e contam com a ajuda de Mestre dos Magos para tentar voltar para casa.',
+  },
+  {
+    image: './images/digimon.png',
+    title: 'Digimon',
+    description: 'As aventuras de sete crianças presas em um mundo bizarro habitado por monstros digitais.',
+  },
+  {
+    image: './images/dragon-ball.png',
+    title: 'Dragon Ball Z',
+    description: 'As aventuras de um poderoso guerreiro chamado Goku, seu filho Gohan e seus aliados, que se esforçam para defender a Terra das ameaças. Eles são auxiliados por um dragão que concede os desejos de quem quer que reúna as sete Esferas do Dragão.',
+  },
+  {
+    image: './images/hantaro.png',
+    title: 'Hamtaro',
+    description: 'Hamtaro é um anime de sucesso no Japão entre outros países que conta as aventuras de pequenos hamsters que saem de suas gaiolas para se reunir com seus amigos ou ajudar os seus donos.',
+  },
+  {
+    image: './images/jakie-chan.png',
+    title: 'Jakie Chan',
+    description: 'Jackie Chan é um arqueólogo especializado em artes marciais que levava uma vida tranquila até encontrar um talismã. Ele descobre que esse é o primeiro de 12 artefatos procurados pela Mão Negra, uma cruel organização secreta que pretende reviver um espirito antigo. Em suas lutas contra as forças do mal, ele conta com o apoio de sua inteligente e corajosa sobrinha Jade e de seu sábio e ranzinza tio Chan.',
+  },
+  {
+    image: './images/kim-possible.png',
+    title: 'Kim Possible',
+    description: 'Kim Possible parece ser uma adolescente normal, mas salva o mundo dos vilões em seu tempo livre.',
+  },
+  {
+    image: './images/monica.png',
+    title: 'Turma da Mônica',
+    description: 'Um grupo de amigos está sempre se metendo em confusões no Bairro do Limoeiro. Acompanhe todas as aventuras de Mônica, Cebolinha, Cascão, Magali e do resto da turminha. A atração é baseada nos quadrinhos criados por Maurício de Souza.',
+  },
+  {
+    image: './images/pica-pau.png',
+    title: 'Pica Pau',
+    description: 'O pássaro falante Pica-Pau sempre tenta se dar bem em qualquer situação. Na companhia de seu amigo, o cavalo medroso Pé de Pano, ele adora pregar peças em seus inimigos Zeca Urubu, Zé Jacaré, Meany Ranheta, entre outros.',
+  },
+  {
+    image: './images/pokemon.png',
+    title: 'Pokemon',
+    description: 'O desenho acompanha a jornada de Ash Ketchum, um corajoso garoto da cidade de Pallet, que sonha se tornar um Mestre Pokémon.',
+  },
+  {
+    image: './images/sailor-moon.png',
+    title: 'Sailor Moon',
+    description: 'Sinopse. Sailor Moon conta a história de Usagi Tsukino, uma garota normal e inocente de 14 anos — pelo menos, é isso que ela pensa — que um dia encontra Luna, uma gata falante que revela a identidade de Usagi como "Sailor Moon", uma guerreira mágica destinada a salvar a Terra das forças do mal.',
+  },
+  {
+    image: './images/sakura.png',
+    title: 'Cardcaptor Sakura',
+    description: 'Sakura Kinomoto, uma garota de 10 anos de idade, vê sua vidar ao tropeçar em um livro peculiar em uma biblioteca e espalhar um conjunto de cartas mágicas, as chamadas Cartas Clow.',
+  },
+  {
+    image: './images/super-choque.png',
+    title: 'Super Choque',
+    description: 'Virgil Hawkins é um jovem inteligente e bem-humorado que vive na cidade de Dakota, com o pai e a irmã. Após ser, acidentalmente, exposto a um gás desconhecido, ele ganha super poderes eletrostáticos.',
+  },
+  {
+    image: './images/timao-e-pumba.png',
+    title: 'Timão e Pumba',
+    description: 'O adorável javali Pumba e seu camarada suricata Timão mantêm um estilo de vida sem preocupações "hakuna matata", que lhes traz muitos problemas na selva.',
+  },
+  {
+    image: './images/tom-e-jerry.png',
+    title: 'Tom e Jerry',
+    description: 'Tom é um gato determinado, porém impaciente, cujo objetivo de vida é capturar o ratinho Jerry. Porém, o pequeno animal é inteligente e cheio de habilidades. O clássico desenho acompanha as loucas e eletrizantes perseguições entre gato e rato.',
+  },
+  {
+    image: './images/ursinhos.png',
+    title: 'Ursinhos Carinhosos',
+    description: ' A Série se passa na terra do carinho, onde moram os Ursinhos Carinhosos, em cada episódio eles ensinam o valor da amor, da amizade, da fidelidade e a compartilhar felicidades uns com os outros e eles sempre tentam superar as maldades do Ursinho Roncador',
+  },
+  {
+    image: './images/yugioh.png',
+    title: 'Yu-Gi-Oh',
+    description: 'O jovem estudante do ensino médio Yugi Muto derrota o campeão mundial, Seto Kaiba, em um duelo de cartas com a misteriosa ajuda do quebra-cabeça Millenium. Em razão de sua vitória inesperada, Yugi se torna famoso em todo o mundo e passa a participar de outros duelos para salvar os amigos e a família.',
+  },
+  {
+    image: './images/zodiaco.png',
+    title: 'Cavaleiros do Zodíaco',
+    description: 'Saint Seiya (nome original da série) conta a história de um grupo de cinco jovens guerreiros cuja função era proteger Saori Kido, reencarnação de Atena, a deusa grega da sabedoria.',
+  },
+];
