238205aae9656ec1837788f8745e08df24f7f83e
370isa <eloisaamorin370@gmail.com>
2020-02-11 09:30:36 -0300

    #3: ajusta a estrutura html, e altera nome dos scripts

---
+++ js/scheduleTime.js
@@ -0,0 +1,9 @@
+const scheduleTime = $('.schedule-block-time .schedule-time');
+const btnHour = $('.container .schedule-hour button');
+
+// percorre os botões
+for (const btn of btnHour) {
+  // procura o botão com a classe hour-now
+  
+}
+// scheduleTime com as mesmas informações
